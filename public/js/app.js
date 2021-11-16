da128f43f98679af32980d76f97d3a3fe0edf441
Johanna Osterrieter <info@johannaost.de>
2021-11-16 11:12:44 +0100

    basic upload setup

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,37 @@ Vue.createApp({
     data() {
         return {
             images: [],
+            title: "",
+            desc: "",
+            username: "",
+            file: null,
         };
     },
     mounted: function () {
         fetch("/images.json")
             .then((images) => images.json())
             .then((images) => {
-                console.log("images coming back", images.rows);
+                // console.log("images coming back", images.rows);
                 this.images = images.rows;
             });
+    },
+    methods: {
+        setFile(e) {
+            // method to grab the input.files property and
+            // set it as the value of this.file
+            this.file = e.target.files[0];
+        },
+        upload() {
+            const formData = new FormData();
+            // append the information we wand to send along in
+            // smaller bits (as a new FormData)
+            formData.append("file", this.file);
+            formData.append("title", this.title);
+            formData.append("desc", this.desc);
+            fetch("/upload", {
+                method: "POST",
+                body: formData,
+            });
+        },
     },
 }).mount("#main");
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//============================ SERVER SETUP ============================//
+
 const express = require("express");
 const app = express();
 const db = require("./db.js");
@@ -6,10 +8,45 @@ app.use(express.static("./public"));
 
 app.use(express.json());
 
+//============================ MULTER SETUP ============================//
+
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
+//============================ ROUTES ============================//
+
+app.post("/upload", uploader.single("file"), function (req, res) {
+    if (req.file) {
+        // it worked!
+        res.sendStatus(200);
+    } else {
+        // boo hoo
+        res.sendStatus(500);
+    }
+});
+
 app.get("/images.json", (req, res) => {
     db.getImages()
         .then((images) => {
-            console.log(images);
             res.json(images);
         })
         .catch((err) => {
